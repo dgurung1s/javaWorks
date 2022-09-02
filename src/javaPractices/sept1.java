e44e00ef0e51e63e07eff2256944467ad5ba7da6
dgurung1s <donnagrg5@gmail.com>
2022-09-01 23:12:50 -0500

    sept1st display 1-10

--- src/javaPractices/sept1.java
+++ src/javaPractices/sept1.java
@@ -4,6 +4,10 @@ public class sept1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		for (int a = 1; a <= 10; a = a+1) {
+			System.out.println(a);
+		}
 
 	}
 
