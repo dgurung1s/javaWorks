c21e6542858cae441f1eb12aacfa31ea0ee99f5f
Dona Gurung <donnagrg5@gmail.com>
2022-09-03 23:55:24 -0500

    Started with comparable and reverse

--- src/javaPractices/sept1.java
+++ src/javaPractices/sept1.java
@@ -4,6 +4,25 @@ public class sept1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+	int a = 25;
+	int b = 39;
+	if (a != b);
+	System.out.println("25 != 39");
+	if (a < b);
+	System.out.println("25 < 39");
+	if(a <= b);
+	System.out.println("25 <= 39");
+
+	String rev = " ";
+	String str1 = "The quick brown fox";
+	for (int x = 0; x < str1.length(); x++){
+  	rev = str1.charAt(x) + rev;
+	}
+	System.out.println(rev);
+	System.out.println(str1.length());
+
+
+
 
 	}
 
